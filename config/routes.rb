22569c2319e2bc0313253b19e67c2de687892013
hirokimac <hiroki.yokomizo1030@gmail.com>
2021-01-07 23:10:32 +0900

    20210107_bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  resources :books
+  get "books/new"
+  get "books" => "books#index"
+  post "books" => "books#create"
+  get "books/:id/edit" => "books#edit" , as: "edit_book"
+  get "books/:id" => "books#show" , as: "book"
+  patch "books/:id" => "books#update"
+  put "books/:id" => "books#update"
+  delete "books/:id" => "books#destroy" , as: "destroy_book"
 end
